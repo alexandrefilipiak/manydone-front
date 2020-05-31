bae2df9fd44a6352f76b0f933f2380856a2e9621
Alexandre Filipiak <a.filipiak@gmail.com>
2020-05-31 19:53:42 +0400

    [refactor] dates are now in moment format

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import moment from "moment";
 
 export const donesReducer = () => {
   return [
@@ -7,32 +8,32 @@ export const donesReducer = () => {
       title: "Certification PMP",
       description:
         "Project Management Professional, above target for every domain",
-      doneDate: new Date(),
+      doneDate: moment("25/12/2019", "DD-MM-YYYY"),
     },
     {
       id: 2,
       title: "Certification PMI-ACP",
       description:
         "Project Management Institute - Agicle Certifide Professional, above target for every domain",
-      doneDate: new Date(),
+      doneDate: moment("25/12/2019", "DD-MM-YYYY"),
     },
     {
       id: 3,
       title: "Core de Force, round 1",
       description: "Fitness program",
-      doneDate: new Date(),
+      doneDate: moment("25/12/2019", "DD-MM-YYYY"),
     },
     {
       id: 4,
       title: "Core de Force, round 2",
       description: "Fitness program",
-      doneDate: new Date(),
+      doneDate: moment("25/12/2019", "DD-MM-YYYY"),
     },
   ];
 };
 
 export const selectedDoneReducer = (selectedDone = null, action) => {
-  if (action.type === "SELECTED_DONE") {
+  if (action.type === "DONE_SELECTED") {
     return action.payload;
   }
 
