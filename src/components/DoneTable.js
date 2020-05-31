90dc3eb870a3533fd91308819050aeab9aaec40c
Alexandre Filipiak <a.filipiak@gmail.com>
2020-05-31 19:48:51 +0400

    [feat] Initial version of the done table

---
+++ src/components/DoneTable.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectDone } from "../actions";
+
+class DoneTable extends React.Component {
+  renderList() {
+    return this.props.dones.map((done) => {
+      return (
+        <div className="item" key={done.id}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectDone(done)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{done.title}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return { dones: state.dones };
+};
+
+export default connect(mapStateToProps, { selectDone })(DoneTable);
