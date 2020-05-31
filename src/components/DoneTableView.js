ddf11f05e9c51a13dddb6b0b20001b9a1cb4751b
Alexandre Filipiak <a.filipiak@gmail.com>
2020-05-31 19:46:49 +0400

    [feat] Done Table view is now split in 2 components
    Component DoneTable on the left
    Component DoneDetail on the right

--- src/components/DoneTableView.js
+++ src/components/DoneTableView.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { connect } from "react-redux";
+import DoneDetail from "./DoneDetail.js";
+import DoneTable from "./DoneTable.js";
 
 class DoneTableView extends React.Component {
   render() {
-    return <div>Dones</div>;
+    return (
+      <React.Fragment>
+        <div className="ui container grid">
+          <div className="ui row">
+            <div className="column height wide">
+              <DoneTable />
+            </div>
+            <div className="column height wide">
+              <DoneDetail />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  console.log("here is the state:" + JSON.stringify(state));
-
-  return state;
-};
-
-export default connect(mapStateToProps)(DoneTableView);
+export default DoneTableView;
