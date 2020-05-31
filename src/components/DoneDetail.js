84e54cd9c6f146e68f01d1f38dd99c8421eca493
Alexandre Filipiak <a.filipiak@gmail.com>
2020-05-31 19:49:29 +0400

    [feat] Initial version of the done detail
    This displays the details of a done, in the right side of the table
    list.

---
+++ src/components/DoneDetail.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const DoneDetail = ({ done }) => {
+  if (!done) {
+    return <div>Please select a done!</div>;
+  }
+  return (
+    <div>
+      <h3>>Details for:</h3>
+      <p>
+        Title: {done.title}
+        <br />
+        Description: {done.description}
+        <br />
+        Date: {done.doneDate.format("DD/MM/YYYY")}
+        <br />
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return { done: state.selectedDone };
+};
+
+export default connect(mapStateToProps)(DoneDetail);
