88af1d0958f0ae59bf402e795b02609d759dbd8b
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-02 22:24:14 +0400

    [feat] added wiring of redux-thunk to redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
       <App />
     </Provider>
   </React.StrictMode>,
