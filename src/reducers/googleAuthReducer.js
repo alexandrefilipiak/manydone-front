56f4e895a774edc79a2aa18bfa2ee06828329d38
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-04 23:48:02 +0400

    [feat] adding our Google auth reducer

---
+++ src/reducers/googleAuthReducer.js
@@ -0,0 +1,18 @@
+import { relativeTimeRounding } from "moment";
+import { SIGN_IN, SIGN_OUT } from "../actions/types.js";
+
+const INITIAL_STATE = {
+  isSignedIn: null,
+  userId: null,
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
+    default:
+      return state;
+  }
+};
