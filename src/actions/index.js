35068a4ef8f30dbc9ac39aa963802b7870d64a25
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-04 23:50:27 +0400

    [feat] added actionCreators for Google auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,23 @@
-// Action creator
+import { SIGN_IN, SIGN_OUT } from "./types.js";
+// Action creators
 export const selectDone = (doneId) => {
   //Return an action
   return {
     type: "DONE_SELECTED",
     payload: doneId,
+  };
+};
+export const signIn = (userId) => {
+  //Return an action
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
+
+export const signOut = () => {
+  //Return an action
+  return {
+    type: SIGN_OUT,
   };
 };
