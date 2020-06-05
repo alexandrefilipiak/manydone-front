783c70a2f7b3fc137fa9abdc2ae39a7c9117359b
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-05 09:15:00 +0400

    [feat] Wiring up Redux Dev Tools extension

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+
 import App from "./components/App";
 import reducers from "./reducers";
-import { applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
