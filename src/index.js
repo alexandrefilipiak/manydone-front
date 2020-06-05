4e04cdba8937d650c6736c64c4344228850a7216
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-05 10:11:52 +0400

    [fix} Removed Strict Mode, which was causing warning pbs

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import thunk from "redux-thunk";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
