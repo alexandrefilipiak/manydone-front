c512c7fdfeb47baa0a43a8a6b5d191dd784cb3d6
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-06 22:58:44 +0400

    [feat] added Field components from redux-form library

--- src/components/dones/DoneCreate.js
+++ src/components/dones/DoneCreate.js
@@ -2,9 +2,18 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class DoneCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
   render() {
-    console.log(this.props);
-    return <div>create a done</div>;
+    //console.log(formProps);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    ); //<Field name="doneDate" />
   }
 }
 
