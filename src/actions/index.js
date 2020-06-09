cd10a12206b682c270cbf5f10fd8d7206b4304c8
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-09 23:45:26 +0400

    [feat] added new action creator createDone

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from "./types.js";
+import dones from "../apis/dones.js";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_DONE,
+  FETCH_DONES,
+  FETCH_DONE,
+  DELETE_DONE,
+  EDIT_DONE,
+} from "./types.js";
 // Action creators
 export const selectDone = (doneId) => {
   //Return an action
@@ -20,4 +29,13 @@ export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
+};
+
+export const createDone = (formValues) => async (dispatch) => {
+  const response = await dones.post("/dones", formValues);
+  //Return an action
+  dispatch({
+    type: CREATE_DONE,
+    payload: response.data,
+  });
 };
