a6a19b7a71c8da34f9f3722a0fec9980b48edff3
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-09 23:46:12 +0400

    [feat] added types for 4 new action creators

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUt";
+export const CREATE_DONE = "CREATE_DONE";
+export const FETCH_DONES = "FETCH_DONES";
+export const FETCH_DONE = "FETCH_DONE";
+export const DELETE_DONE = "DELETE_DONE";
+export const EDIT_DONE = "EDITDELETE_DONE";
