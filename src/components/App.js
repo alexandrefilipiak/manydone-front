45d84ba38a9b50f3e1db2c478194e51b95eb3403
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-22 23:43:48 +0400

    [feat] replacing BrowserRouter by Router
    This will enable us to manage our own history object.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
+import { Switch, Router, Route } from "react-router-dom";
 import Welcome from "./Welcome.jsx";
 import DoneQuickView from "./dones/DoneQuickView.jsx";
 import DoneTableView from "./dones/DoneTableView.js";
 import DoneCreate from "./dones/DoneCreate.js";
 import Header from "./Header.jsx";
+import history from "../history";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route path="/" exact component={Welcome} />
           <Route path="/done-quick-view" component={DoneQuickView} />
           <Route path="/done-table-view" component={DoneTableView} />
           <Route path="/done-create" component={DoneCreate} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
