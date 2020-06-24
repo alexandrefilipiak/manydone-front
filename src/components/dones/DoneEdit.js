386976b9496d26034d7a546ba101bfb18b18e7e6
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-24 09:29:38 +0400

    [feat] added new page DoneEdit

---
+++ src/components/dones/DoneEdit.js
@@ -0,0 +1,15 @@
+// generated with template rcredux
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export class DoneEdit extends Component {
+  render() {
+    return <div>DoneEdit</div>;
+  }
+}
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DoneEdit);
