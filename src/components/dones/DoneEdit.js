46f6cc1dcb96df4d7f76bf62456d2aeb3f70f868
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-27 19:14:53 +0400

    [refactor] centralization of the form in DoneForm

--- src/components/dones/DoneEdit.js
+++ src/components/dones/DoneEdit.js
@@ -1,19 +1,42 @@
 // generated with template rcredux
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchDone } from "../../actions/index";
+import { fetchDone, editDone } from "../../actions/index";
+import DoneForm from "./DoneForm";
 
 class DoneEdit extends React.Component {
   componentDidMount() {
     this.props.fetchDone(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {
+    console.log("test");
+    this.props.editDone(this.props.match.params.id, formValues);
+  };
+
   render() {
     console.log(this.props);
+    console.log("here is this.props.done");
+    console.log(this.props.done);
     if (!this.props.done) {
       return <div>Loading...</div>;
     }
 
-    return <div>{this.props.done.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Done</h3>
+        <DoneForm
+          initialValues={_.pick(
+            this.props.done,
+            "title",
+            "description",
+            "doneDate"
+          )}
+          onSubmit={this.onSubmit}
+        ></DoneForm>
+      </div>
+    );
   }
 }
 
@@ -23,6 +46,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = {
   fetchDone,
+  editDone,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DoneEdit);
