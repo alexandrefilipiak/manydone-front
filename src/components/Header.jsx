c3f38938bf66b912c6b5ff541ae6c2059b707c9e
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-27 19:08:03 +0400

    [refactor] remove useless constructor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import GoogleAuth from "./GoogleAuth";
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="ui secondary pointing menu">
