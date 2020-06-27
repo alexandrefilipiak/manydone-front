6804d705bfe5694ac14638de036d2002af66fcc1
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-27 19:10:01 +0400

    [refactor] removed useless constructor

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Welcome extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <h1>
