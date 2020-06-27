dab53d04644dac5f28a4682e58aea976659f005e
Alexandre Filipiak <a.filipiak@gmail.com>
2020-06-27 19:13:07 +0400

    [refactor] removed useless constructor

--- src/components/dones/DoneQuickView.jsx
+++ src/components/dones/DoneQuickView.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 export default class DoneQuickView extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return <h1>Done Quick View</h1>;
   }
