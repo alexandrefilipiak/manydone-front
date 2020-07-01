c9681d2364fb580fd074fc445f462277a98dbdb9
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-01 22:43:40 +0400

    [feat] Added new done delete component

---
+++ src/components/dones/DoneDelete.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Modal from "../Modal";
+
+const DoneDelete = () => {
+  return (
+    <div>
+      StreamDelete
+      <Modal />
+    </div>
+  );
+};
+
+export default DoneDelete;
