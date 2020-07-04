b716d6b62080e2fa16586da24c5e521da5650426
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-04 20:17:13 +0400

    [feat] Now Modal cancels when clicking outside it

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
+    <div
+      className="ui dimmer modals visible active"
+      onClick={() => history.push("/done-table-view")}
+    >
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className="ui standard modal visible active"
+      >
         <div className="header">Delete Done</div>
         <div className="content">
           Are you sure you want to delete this done?
