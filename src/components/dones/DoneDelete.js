efc9d34af96ea9398d5f5f7890545477dbb3c9af
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-05 23:32:15 +0400

    {feat} Make delete modal generic

--- src/components/dones/DoneDelete.js
+++ src/components/dones/DoneDelete.js
@@ -2,10 +2,22 @@ import React from "react";
 import Modal from "../Modal";
 
 const DoneDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        redirectPath="/"
+        title="Delete Done"
+        content="Are you sure you want to delete this done?"
+        actions={actions}
+      />
     </div>
   );
 };
