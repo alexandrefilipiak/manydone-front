f2ad7b593c9e7fe64752b228943ced5cfded0352
Alexandre Filipiak <a.filipiak@gmail.com>
2020-07-07 22:15:12 +0400

    [feat] Done delete modal displays the title of the done

--- src/components/dones/DoneDelete.js
+++ src/components/dones/DoneDelete.js
@@ -1,26 +1,38 @@
 import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
+import { fetchDone } from "../../actions";
+import { connect } from "react-redux";
 
-const DoneDelete = () => {
-  const actions = (
-    <>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </>
-  );
+class DoneDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchDone(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      StreamDelete
+  render() {
+    const actions = (
+      <>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </>
+    );
+    return (
       <Modal
         onDismiss={() => history.push("/done-table-view")}
         title="Delete Done"
-        content="Are you sure you want to delete this done?"
+        content={`Are you sure you want to delete the done item with title: ${
+          this.props.done ? this.props.done.title : ""
+        }?`}
         actions={actions}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    done: state.dones[ownProps.match.params.id],
+  };
 };
 
-export default DoneDelete;
+export default connect(mapStateToProps, { fetchDone })(DoneDelete);
